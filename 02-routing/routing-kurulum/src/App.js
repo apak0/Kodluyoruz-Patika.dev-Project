8921d3aa6c5d56e2679d2959dadef5153c9db657
Muhammed Apak <79720402+apak0@users.noreply.github.com>
2022-10-25 23:00:56 +0300

    Routing kurulumu

---
+++ 02-routing/routing-kurulum/src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Users from "./components/Users";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+
+export default App;
