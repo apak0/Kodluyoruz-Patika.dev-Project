d9725cd8e218782338fba84e944ad5fcd19ea70e
Muhammed Apak <79720402+apak0@users.noreply.github.com>
2022-10-29 19:57:03 +0300

    Form validations with yup library

--- 03-formik/src/App.js
+++ 03-formik/src/App.js
@@ -1,125 +1,15 @@
 import "./App.css";
+import Signup from "./components/Signup";
 
-import { Formik, useFormik} from "formik";
 
 function App() {
 
   
-    const {handleSubmit, handleChange, values} = useFormik({
-      initialValues: {
-        firstName: "Muhammed",
-        lastName: "Apak",
-        email: "muhampak@gmail.com",
-        gender: "male",
-        hobies: [],
-        country: "Türkiye",
-      },
-      onSubmit:(values) => {
-        console.log(values);
-      }
-    });
+   
 
   return (
     <div className="App">
-      <h1>Sign Up</h1>
-     
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="firstName">First Name</label>
-            <input
-              name="firstName"
-              value={values.firstName}
-              onChange={handleChange}
-            />
-            <br />
-            <br />
-
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              name="lastName"
-              value={values.lastName}
-              onChange={handleChange}
-            />
-            <br />
-            <br />
-
-            <label htmlFor="email">Email</label>
-            <input name="email" value={values.email} onChange={handleChange} />
-            <br />
-            <br />
-
-            <span>Male</span>
-            <input
-              type="radio"
-              name="gender"
-              value="male"
-              onChange={handleChange}
-              checked={values.gender === "male"}
-            />
-
-            <span>Female</span>
-            <input
-              type="radio"
-              name="gender"
-              value="female"
-              onChange={handleChange}
-              checked={values.gender === "female"}
-            />
-            <br />
-            <br />
-
-            <div>
-              <input
-                type="checkbox"
-                name="hobies"
-                value="Football"
-                onChange={handleChange}
-              />
-              Football
-            </div>
-
-            <div>
-              <input
-                type="checkbox"
-                name="hobies"
-                value="Climbing"
-                onChange={handleChange}
-              />
-              Climbing
-            </div>
-
-            <div>
-              <input
-                type="checkbox"
-                name="hobies"
-                value="Surf"
-                onChange={handleChange}
-              />
-              Surf
-            </div>
-
-            <br />
-            <br />
-
-            <select
-              name="country"
-              value={values.country}
-              onChange={handleChange}
-            >
-              <option value="türkiye">Türkiye</option>
-              <option value="england">England</option>
-              <option value="usa">USA</option>
-            </select>
-
-            <br />
-            <br />
-
-            <button type="submit">Submit</button>
-
-            <br />
-            <br />
-
-            {JSON.stringify(values)}
-          </form>
+      <Signup/>
      
     </div>
   );
---
+++ 03-formik/src/components/validations.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as yup from 'yup';
+
+
+const validations = yup.object({
+   
+    email: yup.string().email().required(),
+    password: yup.string().min(5).required(),
+    passwordConfirm: yup.string().oneOf([yup.ref("password")]).required(),
+   
+  });
+
+export default validations;
