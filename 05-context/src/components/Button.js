3cd3b9b89a12ab3f4c0b2654c092bd8a0c420442
Muhammed Apak <79720402+apak0@users.noreply.github.com>
2022-10-30 21:36:54 +0300

    Created Custon Hook

--- 05-context/src/components/Button.js
+++ 05-context/src/components/Button.js
@@ -1,9 +1,7 @@
-import {useContext} from 'react'
-
-import ThemeContext from '../context/ThemeContext'
+import {useTheme} from '../context/ThemeContext'
 
 function Button({children}) {
-    const {theme, setTheme} = useContext(ThemeContext);
+    const {theme, setTheme} = useTheme();
 
    
 
