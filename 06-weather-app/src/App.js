fe5f55e2b16e6f20032ccf337a90190cee6c617f
Muhammed Apak <79720402+apak0@users.noreply.github.com>
2022-11-01 21:33:29 +0300

    Addes API key

--- 06-weather-app/src/App.js
+++ 06-weather-app/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Container from './components/Container';
+import {useState} from "react"
 
 function App() {
+  
+
+  const apiKey = "795f39038604487d46886d1107c6071b"
+  const [weatherData, setWeatgerData] = useState({})
+  const [city, setCity] = useState("")
+
+  const getWeather = (event) => {
+    if(event.key == "Enter") {
+      fetch()
+    }
+  }
+
   return (
     <div className="App">
       <Container/>
+
+
     </div>
   );
 }
