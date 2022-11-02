46aab081e8338b0c9c1bb79103ad59802b0c8fef
Muhammed Apak <79720402+apak0@users.noreply.github.com>
2022-11-02 23:25:22 +0300

    API url and API key merge

--- 06-weather-app/src/App.js
+++ 06-weather-app/src/App.js
@@ -2,22 +2,24 @@ import './App.css';
 import Container from './components/Container';
 import {useState} from "react"
 
+import WeatherContext from './context/Weather';
+
 function App() {
   
-
+  const apiUrl = "https://api.openweathermap.org/data/2.5/"
   const apiKey = "795f39038604487d46886d1107c6071b"
   const [weatherData, setWeatgerData] = useState({})
-  const [city, setCity] = useState("")
+  
 
-  const getWeather = (event) => {
-    if(event.key == "Enter") {
-      fetch()
-    }
-  }
+  const getWeather = `${apiUrl}weather?q=city&appid=${apiKey}&units=metric&lang=tr`
+  
+  console.log(getWeather);
 
   return (
     <div className="App">
-      <Container/>
+      <WeatherContext.Provider value="" >
+        <Container/>
+      </WeatherContext.Provider>
 
 
     </div>
--- 06-weather-app/src/components/Container.js
+++ 06-weather-app/src/components/Container.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import {useState} from 'react'
 import Button from './Button'
 import Content from './Content'
 
 function Container() {
+
+  const [city, setCity] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setCity(e.target.value)
+    console.log(e.target.value);
+  }
+
   return (
     <div style={{backgroundColor:"crimson", border:"2px solid blue"}} >
+      <form onSubmit={handleSubmit} >
+      <input type="text" placeholder='Şehir Giriniz'  ></input>
+      </form>
         <Button/>
         <Content/>
     </div>
