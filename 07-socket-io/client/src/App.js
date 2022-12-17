238805e618c20ac4aeceb641e898b57783da52f4
Muhammed Apak <79720402+apak0@users.noreply.github.com>
2022-12-17 22:49:22 +0300

    for socket.io

--- 07-socket-io/client/src/App.js
+++ 07-socket-io/client/src/App.js
@@ -2,7 +2,15 @@
 import './App.css';
 import Palette from './components/Palette';
 
+import {useEffect} from 'react';
+import {init} from "./socketApi"
+
 function App() {
+
+  useEffect(() => {
+    init();
+  }, [])
+
   return (
     <div className="App">
       <Palette />
