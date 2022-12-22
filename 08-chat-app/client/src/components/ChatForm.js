7dd5cc078cf57d6bb1fbfa2d8cbfd872bb6d87e9
Muhammed Apak <79720402+apak0@users.noreply.github.com>
2022-12-22 21:37:36 +0300

    chat context created

--- 08-chat-app/client/src/components/ChatForm.js
+++ 08-chat-app/client/src/components/ChatForm.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState } from "react";
+import styles from "./styles.module.css";
 
 function ChatForm() {
+  const [message, setMessage] = useState("");
+
+  const handleSubbmit = (e) => {
+    e.preventDefault();
+    console.log(message.trim())
+    setMessage("")
+  }
+
   return (
-    <div>ChatForm</div>
-  )
+    <div>
+      <form onSubmit={handleSubbmit}>
+        <input
+          className={styles.textInput}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
 
-export default ChatForm
+export default ChatForm;
--- 08-chat-app/client/src/context/ChatContext.js
+++ 08-chat-app/client/src/context/ChatContext.js
@@ -1,18 +1,19 @@
-import { createContext, useState } from "react" 
+import { createContext, useState, useContext } from "react";
 
 const ChatContext = createContext();
 
-export const ChatProvider = ({children}) => {
+export const ChatProvider = ({ children }) => {
+  const [messages, setMessages] = useState([
+    { message: "selam" },
+    { message: "naber" },
+  ]);
 
-    const [messages, setMessages] = useState([]);
+  const values = {
+    messages,
+    setMessages,
+  };
 
-    const values= {
-        messages,
-        setMessages,
-    };
-
-    return <ChatContext.Provider value = {values}>{children}</ChatContext.Provider>;
-
+  return <ChatContext.Provider value={values}>{children}</ChatContext.Provider>;
 };
 
-export default ChatContext;
+export const useChat = () => useContext(ChatContext)
