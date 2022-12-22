7c8491f411518262e357dfb26e259b18f84c5f67
Muhammed Apak <79720402+apak0@users.noreply.github.com>
2022-12-22 22:19:25 +0300

    backend connection

--- 08-chat-app/client/src/components/Container.js
+++ 08-chat-app/client/src/components/Container.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ChatList from './ChatList'
 import ChatForm from './ChatForm'
+import {init} from "../sockeApi"
 
 
 function Container() {
+
+
+  useEffect(() => {
+    init();
+  }, [])
+
   return (
     <div className='App' >
         <ChatList/>
