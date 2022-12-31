2bbfdca6dc4dcb4fe6d69e31e3249455ff1388d8
Muhammed Apak <79720402+apak0@users.noreply.github.com>
2022-12-31 22:33:34 +0300

    test file changed

--- 11-emoji-search/src/App.test.js
+++ 11-emoji-search/src/App.test.js
@@ -6,3 +6,22 @@ it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
+
+
+
+test("Input ve button dökümanda bulunmalı", () => {
+  expect(button).toBeInTheDocument();
+  expect(input).toBeInTheDocument();
+});
+
+test("Inputa string girilip butona basılınca listeye eklenmeli", () => {
+  //inputu doldur
+  const name = "Mehmet";
+  userEvent.type(input, name);
+
+  //buttona tıkla
+  userEvent.click(button);
+
+  //assertion
+  expect(screen.getByText(name)).toBeInTheDocument();
+})
