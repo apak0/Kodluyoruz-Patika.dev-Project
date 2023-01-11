54d6555c0267c275618d3cfe3354347f86ed43be
Muhammed Apak <79720402+apak0@users.noreply.github.com>
2023-01-11 22:37:05 +0300

    Publishing_0
    Sample Package upload to npmjs.com

---
+++ 12-publishing/simple task/src/App.js
@@ -0,0 +1,16 @@
+
+import { Button, ExampleComponent } from 'myuimanager';
+import './App.css';
+import "myuimanager/dist/index.css"
+
+
+function App() {
+  return (
+    <div className="App">
+     <Button text="selam"/>
+     <ExampleComponent text="Selam ben example componentiyim"/>
+    </div>
+  );
+}
+
+export default App;
